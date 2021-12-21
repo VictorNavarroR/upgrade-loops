d489f9f3135eba5f6cf93fbf1fa80f3ea0fd40aa
Victor Navarro <victor.navarro@bootcamp-upgrade.com>
2021-12-21 18:05:18 +0100

    Iteration#1

---
+++ index.js
@@ -0,0 +1,8 @@
+//Iteration#1
+const products = ['Camiseta de Pokemon', 'Pantalón coquinero', 'Gorra de gansta', 'Camiseta de Basket', 'Cinrurón de Orión', 'AC/DC Camiseta'];
+
+products.forEach( item => {
+    if(item.includes('Camiseta')) {
+        console.log(item)
+    }
+});
